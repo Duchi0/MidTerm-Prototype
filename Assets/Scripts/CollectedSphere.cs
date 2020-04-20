a4be37662ad752cf260277ae8f03003b6f543b25
Duchi0 <61675359+Duchi0@users.noreply.github.com>
2020-04-19 22:57:54 -0500

    Added Player UI and updated scripts

--- Assets/Scripts/CollectedSphere.cs
+++ Assets/Scripts/CollectedSphere.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class CollectedSphere : MonoBehaviour
 {
+    public TextMeshProUGUI sphereCount;
+    private int spheresCollected;
+
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        UpdateScore();
+    }
+    public void UpdateScore()
+    {
+        spheresCollected++;
+        sphereCount.text = $"Spheres Collected: {spheresCollected}/5 ";
     }
 }
